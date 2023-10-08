93ce063c708c7dbb7ecfdd6f90bb1784f4133ada
MenGonz <semendozag@unal.edu.co>
2023-10-07 23:14:18 -0500

    cambios
    lhig

--- estructuras/fila_enlistada.py
+++ estructuras/fila_enlistada.py
@@ -1,20 +1,38 @@
 
-class fila_enlistada(list):
-    def __init__(self, dato_inicial=None):
-        if dato_inicial is not None:
-            self.append(dato_inicial)
-    
-    def enqueue(self, datos):
-        self.append(datos)
-    
-    def dequeue(self):
-        if len(self) != 0:
-            return self.pop(0)
+class fila_enlistada():
+    def __init__(self, dato_inicial=None, limite=-1):
+        self.primero = 0
+        self.ultimo = 0
+        if limite > 0:
+            self.fila = tuple(dato_inicial*limite) 
         else:
-            return None
+            self.fila = [dato_inicial]
+            
+    def enqueue(self, data):
+        self.fila[(self.ultimo +1) % len(self.fila)] = data
+        self.ultimo = (self.ultimo + 1) % len(self.fila)
         
+    def dequeue(self):
+        data = self.fila[self.primero]
+        self.primero = (self.primero + 1) % len(self.fila)
+        return data
+    
     def peek(self):
-        if len(self) != 0: 
-            return self[0]
-        else:
-            return None
+        return self.fila[self.primero]
+    
+    def is_empty(self):
+        return self.primero == self.ultimo
+    
+    def is_full(self):
+        return (self.ultimo + 1) % len(self.fila) == self.primero
+    
+    def get_size(self):
+        return (self.ultimo - self.primero) % len(self.fila) + 1
+    
+    def __str__(self):
+        res = ""
+        for i in range(len(self.fila)):
+            res += str((self.primero + i) % len(self.fila)) + " "
+        return res
+    
+    
--- estructuras/pila_enlistada.py
+++ estructuras/pila_enlistada.py
@@ -1,20 +1,40 @@
 
-class pila_enlistada(list):
-    def __init__(self, dato_inicial=None):
-        if dato_inicial is not None:
-            self.append(dato_inicial)
+class pila_enlistada():
+    def __init__(self, dato_inicial=None, limite=-1):
+        self.cola = 0
+        self.limit = limite
+        self.size = 0
+        if limite > 0:
+            self.pila = tuple(dato_inicial*limite) 
+        else:
+            self.pila = [dato_inicial]
     
-    def push(self, datos):
-        self.append(datos)
-    
+    def push(self, data):
+        if self.cola == len(self.pila):
+            if self.limite == -1:
+                self.pila.append(data)
+                self.size += 1
+        else:
+            self.pila[self.cola] = data
+            self.cola += 1
+            self.size += 1
+            
     def pop(self):
-        if len(self) != 0:
-            return self.pop()
-        else:
-            return None
+        if self.cola is not None:
+            ind = self.cola
+            self.pila[ind] = None
+            self.cola -= 1
+            self.size -= 1
+            return self.pila[self.cola]
         
+    def get_size(self):
+        return self.cola+1
+    
     def peek(self):
-        if len(self) != 0: 
-            return self[-1]
-        else:
-            return None
+        return self.pila[self.cola]
+    
+    def __str__(self):
+        res = ""
+        for i in range(self.size):
+            res += str(i) + " "
+        return res
