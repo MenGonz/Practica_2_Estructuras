bd10b9c3e497425cea3c3d506923390a38b47e34
MenGonz <semendozag@unal.edu.co>
2023-10-18 18:17:29 -0500

    Update main.py

--- estructuras/main.py
+++ estructuras/main.py
@@ -1,12 +1,12 @@
 import csv
-from Empleado import *
+from Empleado import Empleado
 Empleados: list[Empleado] = []
 pwd = []
 
 with open("empleados.csv") as datos:
     lector = csv.reader(datos)
     for row in lector:
-        empleado = Empleado(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
+        empleado: Empleado = Empleado(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
         Empleados.append(empleado)
 
 with open("password.csv") as datos:
