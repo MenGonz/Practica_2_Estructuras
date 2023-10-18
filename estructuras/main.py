bda5b4100dfbf18e2f9eec732449b33c9ec81da1
Miguel Quiceno <miguelangel01yo@gmail.com>
2023-10-18 16:23:22 -0500

    sorroscoposco

--- estructuras/main.py
+++ estructuras/main.py
@@ -2,11 +2,12 @@ import csv
 from Empleado import *
 Empleados = []
 pwd = []
-"""with open("empleados.csv") as datos:
+
+with open("empleados.csv") as datos:
     lector = csv.reader(datos)
     for row in lector:
         empleado = Empleado(row[0],row[1],row[2],row[3],row[4],row[5],row[6])
-        Empleados.append(empleado)"""
+        Empleados.append(empleado)
 
 with open("password.csv") as datos:
     lector = csv.reader(datos)
