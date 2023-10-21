18e7fd3ae10eb4b9709d85ad2f7511535254d9a6
Miguel Quiceno <miguelangel01yo@gmail.com>
2023-10-20 19:54:33 -0500

    agregar mensaje

--- estructuras/Almacenamiento.py
+++ estructuras/Almacenamiento.py
@@ -117,11 +117,14 @@ class Almacenamiento:
         el último borrador guardado de la pila de borradores."""
         ...
         
+        
     @staticmethod
     def agregar_mensaje_BD(destinatario,mensaje):
-        """Este método recibe un destinatario y un mensaje ya éste lo guarda en la
-        bandeja de entrada (BD) del destinatario."""
-        ...
+        os.chdir("BD_Mensajes")
+        with open(destinatario.get_id() + "_BA", "a", encoding='UTF8', newline='') as f:
+            writer = csv.writer(f)
+            writer.writerow([mensaje.get_correo_receptor(), mensaje.get_titulo(), f"{mensaje.get_contenido()}",
+                             mensaje.get_correo_emisor(), mensaje.get_fecha_envío(), mensaje.get_hora_envío()])
         
     @staticmethod
     def agregar_borrador_BD(emisor,mensaje):
